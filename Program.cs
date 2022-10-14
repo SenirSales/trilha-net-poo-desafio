51f49f1ba73ae2bce976e6465c81790ea4503660
Senir Sales Oliveira <senirsales@hotmail.com>
2022-10-14 13:30:08 -0300

    Update - Atualização do projeto
    Atualização do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+
+Smartphone nokia = new Nokia(numero: "(99)95454-4545", modelo: "Smartphone Nokia C20", imei: "121212121212121", memoria: 64);
+nokia.Ligar();
+
+nokia.InstalarAplicativo("Calculadora");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+
+Smartphone iphone = new Iphone(numero: "(99)95454-6464", modelo: "Apple iPhone 13 Pro Max", imei: "313131313131313", memoria: 256);
+iphone.ReceberLigacao();
+
+iphone.InstalarAplicativo("JogoDaForca");
